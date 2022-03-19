8ef1b25a641e016d25cc5d720b1fb13f2ccd92f6
Franatrtur <franta-artur-cech@seznam.cz>
2022-03-19 16:19:17 +0100

    update npm readme

--- src/enc/base64.ts
+++ src/enc/base64.ts
@@ -21,7 +21,7 @@ export default class Base64 implements encoding {
 		str = str.replace(/[^A-Za-z0-9\+\/]/g, "")
 
 		//design based on the example at
-		//https://developer.mozilla.org/en-US/docs/Glossary/Base64 (solution #2)
+		//https://developer.mozilla.org/en-US/docs/Glossary/Base64#appendix_decode_a_base64_string_to_uint8array_or_arraybuffer
 		
 		let outlen = str.length * 3 + 1 >>> 2
 		let bytes = new Uint8Array(outlen)
